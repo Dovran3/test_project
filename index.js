14993bea2b53f61a1031ccd07315588ea147008e
Dovran3 <rahymov.dovran333@gmail.com>
2021-08-03 09:39:43 +0500

    changed

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require("express")
+const express = require("express"),
+      app = express(),
+      port = process.env.PORT || 3000
 
 
-app = express()
 app.use(express.static("files"))
 app.set("view engine", "ejs")
 app.set("views", "files/templates")
 module.exports.app = app
 const routes = require("./files/routes")
 
-const port = process.env.PORT || 3000
+
 app.listen(port, () => {
     console.log(`Server listening on port http:/localhost:${port}...`)
 })
